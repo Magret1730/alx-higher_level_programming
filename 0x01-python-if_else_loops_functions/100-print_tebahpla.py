b560fa5061929b6676dc7c9398dd8a00d8b92214
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-11 21:35:26 -0230

    completed task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-result = ""    
+result = ""
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        #print("{}".format(chr(i)), end="")
         result = result + chr(i)
     elif i % 2 != 0:
-        #print("{}".format(chr(i - 32)), end="")
+        # print("{}".format(chr(i - 32)), end="")
         result = result + chr(i - 32)
-print(result)
+print(result, end="")
