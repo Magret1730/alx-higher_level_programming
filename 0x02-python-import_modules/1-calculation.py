a18a6fbb3e3615bc3fb98c835e0b2aef9f7d958d
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-13 10:58:12 -0230

    Task 1. My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-calculator_add = add(a, b)
-calculator_sub = sub(a, b)
-calculator_mul = mul(a, b)
-calculator_div = div(a, b)
-print("{0} + {1} = {2}".format(a, b, calculator_add))
-print("{} - {} = {}".format(a, b, calculator_sub))
-print(f"{a} * {b} = {calculator_mul}")
-print("{1} / {2} = {0}".format(calculator_div, a, b))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    calculator_add = add(a, b)
+    calculator_sub = sub(a, b)
+    calculator_mul = mul(a, b)
+    calculator_div = div(a, b)
+    print("{0} + {1} = {2}".format(a, b, calculator_add))
+    print("{} - {} = {}".format(a, b, calculator_sub))
+    print("{} * {} = {}".format(a, b, calculator_mul))
+    print("{1} / {2} = {0}".format(calculator_div, a, b))
