0a39cd2efcde1ea4f4713edd8eb9c8b101e550da
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-14 20:43:05 -0230

    Task 4. Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    for i in my_list:
+        this_list = my_list.copy()
+        if i < len(my_list):
+            if idx < 0:
+                return this_list
+            if idx == i - 1:
+                this_list[i - 1] = element
+                return this_list
+        else:
+            return this_list  # copy of original list
