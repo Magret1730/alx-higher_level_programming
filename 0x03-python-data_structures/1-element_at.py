9eb72bd2a5f91b676967aaa6ae55dbe01128fa64
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-14 19:40:35 -0230

    Task 1. Secure access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for i in my_list:
+        if i < len(my_list):
+            if idx < 0:
+                return None
+            if idx == i - 1:
+                return i
+        else:
+            return None
