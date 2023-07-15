8629c318f716b4becab84f355d9c995c5aa94c28
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-15 13:41:31 -0230

    Task 6 modified

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
+        j = 0
         for row in matrix:
             i = 0  # Reset i for each new row
             for element in row:
-                print("{:d}".format(element), end=" ")
+                print("{:d}".format(matrix[j][i]), end=" ")
                 i += 1
             print()
+            j += 1 
