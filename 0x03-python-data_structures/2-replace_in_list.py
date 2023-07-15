c6a3e45d49d46d37410fc4ff58a5673c4a8aab4d
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-15 10:59:41 -0230

    Modified task2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i in my_list:
-        if i < len(my_list):
-            if idx < 0:
-                return my_list
-            elif i - 1 == idx:
-                my_list[i - 1] = element  # insert element at i - 1
-                return my_list
-        else:
-            return my_list
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    else:
+        my_list[idx] = element
+        return my_list
