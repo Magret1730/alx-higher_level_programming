d66c04adafe3ae93bf1ad52c29d92e109b1a1949
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-15 10:03:22 -0230

    Modified task 1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if len(my_list) > 0:
-        for j in my_list:
-            print("{}".format(j))
+    for j in my_list:
+        print("{}".format(j))
