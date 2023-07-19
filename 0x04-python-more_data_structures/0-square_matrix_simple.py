ce7dfd8cb8da6f655e35191a0e8bdf2791893474
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-19 12:23:57 -0230

    fixed error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return matrix
-    if matrix:
+    if matrix is not None:
         for row in matrix:
             if len(row) != len(matrix):
                 return None
         return [[element ** 2 for element in row] for row in matrix]
     else:
         return matrix
-
-# def square_matrix_simple(matrix=[]):
-#    if matrix is None:
-#        return None
-#
-#    if matrix:
-#        # Verify if the matrix is square
-#        for row in matrix:
-#            if len(row) != len(matrix):
-#                return None  # Return None if the input matrix is not square
-#
-#        # Create and return the square matrix
-#        return [[element ** 2 for element in row] for row in matrix]
-#    else:
-#        return None  # Return None if the input matrix is empty
