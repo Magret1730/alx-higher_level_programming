2f64e45b6c943b5bceef24fdd27637f708721718
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-19 13:03:35 -0230

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list:
-        for values in range(len(my_list)):
-            if my_list[values] == search:
-                my_list[values] = replace
-        return my_list
-    else:
-        return my_list
+        # for values in range(len(my_list)):
+#            if my_list[values] == search:
+#                my_list[values] = replace
+        my_list = [replace if element == search else element for element in my_list]
+    return my_list
