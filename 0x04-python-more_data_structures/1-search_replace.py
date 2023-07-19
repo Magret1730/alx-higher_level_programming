9bd89b98c861556d0a30a617591b37365651f3d8
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-19 12:49:58 -0230

    fix error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for values in range(len(my_list)):
-        if my_list[values] == search:
-            my_list[values] = replace
-    return my_list
+    if my_list:
+        for values in range(len(my_list)):
+            if my_list[values] == search:
+                my_list[values] = replace
+        return my_list
+    else:
+        return my_list
