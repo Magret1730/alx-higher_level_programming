c47053ce7a17cc70e226ec2539d6fe9771d0148d
‘Magret’ <oyedeleabiodun@gmail.com>
2023-08-01 18:30:52 -0230

    Task 1
    a class Square that defines a square based on Task 0
    with Private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """a class Square that defines a square"""
+
+    def __init__(self, size):
+        """a module with private instance attribute size"""
+        self.__size__ = size
