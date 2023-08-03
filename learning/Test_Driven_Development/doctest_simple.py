bd0d08a5386f17f9e9b193cc3c0680ba68e771c8
‘Magret’ <oyedeleabiodun@gmail.com>
2023-08-03 17:31:36 -0230

    learning how to use test driven development

---
+++ learning/Test_Driven_Development/doctest_simple.py
@@ -0,0 +1,9 @@
+def my(a, b):
+    """
+    >>> my(2, 3)
+    6
+    >>> my("a", 3)
+    'aaa'
+    """
+
+    return a * b
---
+++ learning/Test_Driven_Development/doctest_simple_with_docs.py
@@ -0,0 +1,15 @@
+def my(a, b):
+    """Returns a * b.
+
+    Works with numbers:
+
+    >>> my(2, 3)
+    6
+
+    Works with strings:
+
+    >>> my('a', 3)
+    'aaa'
+    """
+
+    return a * b
