45d564255f06703f75a2f6f6b38cd8a832bedbe6
‘Magret’ <oyedeleabiodun@gmail.com>
2023-08-07 13:33:57 -0230

    Added Public class attribute print_symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     """
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initializer
@@ -87,7 +87,7 @@ class Rectangle:
         rect = ""
         for i in range(self.__height):
             for j in range(self.__width):
-                rect = rect + '#'
+                rect = rect + str(self.print_symbol)
             if i != (self.__height - 1):
                 rect = rect + '\n'
         return rect
