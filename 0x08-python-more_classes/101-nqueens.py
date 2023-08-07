48ee4334f53ec1a873cf104fae9ad124fca351cb
‘Magret’ <oyedeleabiodun@gmail.com>
2023-08-07 19:31:51 -0230

    not completed

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -7,17 +7,18 @@ non-attacking queens on an N×N chessboard.
 
 import sys
 
-def main():
-"""A program that solves the N queens problem"""
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    
-    try:
-        N = int(sys.argv[1])
-        solve_nqueens(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+def main():
+    """A program that solves the N queens problem"""
+
+    if __name__ == "__main__":
+        if len(sys.argv) != 2:
+            print("Usage: nqueens N")
+            sys.exit(1)
+
+        try:
+            N = int(sys.argv[1])
+            solve_nqueens(N)
+        except ValueError:
+            print("N must be a number")
+            sys.exit(1)
