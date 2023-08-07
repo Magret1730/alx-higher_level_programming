1536a323824bb54a52005c2b247a86137214edd9
‘Magret’ <oyedeleabiodun@gmail.com>
2023-08-07 10:33:20 -0230

    an empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    empty class Rectangle that defines a rectangle
+    """
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+0x08. Python - More Classes and Objects
+
+Learning more on classes and objects
