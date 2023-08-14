dbfe57ee6eb545704547a46279dbf47b547f013e
‘Magret’ <oyedeleabiodun@gmail.com>
2023-08-14 17:43:52 -0230

    Solution to task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module"""
+
+def add_attribute(obj, attribute, value):
+    """
+    adds a new attribute to an object if it’s possible
+
+    Args:
+        obj: The object to which the attribute is to be added.
+        attribute: The name of the attribute to be added.
+        value: The value of the attribute to be added.
+
+    Raises:
+        TypeError: If the object can't have new attributes.
+    """
+
+    if hasattr(obj, '__dict__'):
+        setattr(obj, attribute, value)
+    else:
+        raise TypeError("can't add new attribute")
