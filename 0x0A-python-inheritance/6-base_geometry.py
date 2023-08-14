7d63f3e7b5c87cf0a5868b3c599bf1ffdbf8736e
‘Magret’ <oyedeleabiodun@gmail.com>
2023-08-14 14:04:25 -0230

    Complete solution of task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A class"""
+
+
+class BaseGeometry:
+    """A class with public attribute"""
+
+    def area(self):
+        """raises Exception error"""
+
+        raise Exception("area() is not implemented")
