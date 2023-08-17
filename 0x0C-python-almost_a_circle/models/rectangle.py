e0a5eff9ef5549e1dca5da1788ef7258a3ba9a60
Magret <oyedeleabiodun79@gmail.com>
2023-08-17 11:10:41 -0700

    solution to task 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,8 +17,8 @@ class Rectangle(Base):
             y: 0
             id: id
         """
-
         super().__init__(id)
+
         self.width = width
         self.height = height
         self.x = x
@@ -26,32 +26,60 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter for private instance attribute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter for private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
+        """getter for private instance attribute height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter for private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
+        """getter for private instance attribute x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """setter for private instance attribute x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        """getter for private instance attribute y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setter for private instance attribute y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """returns the area value of the Rectangle instance."""
+        return self.__width * self.__height
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,12 +18,8 @@ class Test_rectangle(unittest.TestCase):
 
     def test_negative_values(self):
         """Negative values test"""
-        r1 = Rectangle(-10, -2, -7, -6, -5)
-        self.assertEqual(r1.id, -5)
-        self.assertEqual(r1.width, -10)
-        self.assertEqual(r1.height, -2)
-        self.assertEqual(r1.x, -7)
-        self.assertEqual(r1.y, -6)
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-10, -2, -7, -6, -5)
 
     def test_large_values(self):
         """Test for large values"""
@@ -43,19 +39,14 @@ class Test_rectangle(unittest.TestCase):
 
     def test_strings(self):
         """Test for strings in the parameters"""
-        r1 = Rectangle("Cohort14", "good", "@", "ing", "rest")
-        self.assertEqual(r1.width, "Cohort14")
-        self.assertEqual(r1.height, "good")
-        self.assertEqual(r1.x, "@")
-        self.assertEqual(r1.y, "ing")
-        self.assertEqual(r1.id, "rest")
+        with self.assertRaises(TypeError):
+            r1 = Rectangle("Cohort14", "good", "@", "ing", "rest")
 
     def test_default_values(self):
         """Test for default values of x and y and id"""
         r1 = Rectangle(34, 56)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
-        self.assertEqual(r1.id, 1)
 
     def test_invalid(self):
         """Test for invalid inputs"""
@@ -76,3 +67,99 @@ class Test_rectangle(unittest.TestCase):
         """Test for x"""
         r1 = Rectangle(3, 5, x=5)
         self.assertEqual(r1.x, 5)
+
+    def test_none_all(self):
+        """Test None for all"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(None, None, None, None)
+
+    # Area test cases
+    def test_area(self):
+        """Test for Area()"""
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+
+    def test_area1(self):
+        """Test for Area1()"""
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
+
+    def test_area_string(self):
+        """Test for width strings"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle("G", 8)
+
+    def test_area_string1(self):
+        """Test for height strings"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(8, "G")
+
+    def test_area_string2(self):
+        """Test for invalid width strings"""
+        with self.assertRaises(NameError):
+            r1 = Rectangle(G, 8)
+
+    def test_area_string3(self):
+        """Test for invalid height strings"""
+        with self.assertRaises(NameError):
+            r1 = Rectangle(8, G)
+
+    def test_area_large_values(self):
+        """Test for large values"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1e676, 15e65)
+
+    def test_area_none(self):
+        """Test for None"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(None, None)
+
+    def test_area_none_width(self):
+        """Test for None on width"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(None, 5)
+
+    def test_area_none_height(self):
+        """Test for None on height"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(5, None)
+
+    def test_area_negative_values_area(self):
+        """Negative values test"""
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-10, -2)
+
+    def test_area_negative_width(self):
+        """Negative values test on width"""
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-10, 2)
+
+    def test_area_negative_height(self):
+        """Negative values test on height"""
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(10, -2)
+
+    def test_area_zero(self):
+        """Test for area zero"""
+        r1 = Rectangle(0, 0)
+        self.assertEqual(r1.area(), 0)
+
+    def test_area_width_zero(self):
+        """Test for area zero width"""
+        r1 = Rectangle(0, 4)
+        self.assertEqual(r1.area(), 0)
+
+    def test_area_height_zero(self):
+        """Test for area zero height"""
+        r1 = Rectangle(5, 0)
+        self.assertEqual(r1.area(), 0)
+
+    def test_area_large_numbers(self):
+        """Test for large area value"""
+        r1 = Rectangle(1000, 10000)
+        self.assertEqual(r1.area(), 1000 * 10000)
+
+    def test_extra_large_numbers_area(self):
+        """Test for very large area value"""
+        r1 = Rectangle((10**6), (10**7))
+        self.assertEqual(r1.area(), (10**6) * (10**7))
