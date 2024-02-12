a8ecd23dc8dc3aea06ea721aefd874d9b80e80c8
Magret <oyedeleabiodun79@gmail.com>
2024-02-12 09:47:27 -0800

    solution to task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// const args = process.argv.slice(2);
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < words.length; i++) console.log(words[i]);
