5338cac804030943ee445e26c70592e20615a09d
Magret <oyedeleabiodun79@gmail.com>
2024-02-13 09:58:59 -0800

    Solution to task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+/*
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      counter += 1;
+    }
+  }
+  return counter;
+}
+*/
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  list.forEach((value) => {
+    if (value === searchElement) {
+      counter++;
+    }
+  });
+  return counter;
+};
