8e51e86dc0b87512399d79fd53373f092280640f
Magret <oyedeleabiodun79@gmail.com>
2024-02-13 09:10:52 -0800

    Resolved the same issue again

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,45 +1,7 @@
 #!/usr/bin/node
-// const Rectangle = require('./4-rectangle');
-
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      Object.create(Rectangle);
-    } else if (w && h) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'X';
-      }
-      console.log(row);
-    }
-  }
-
-  rotate () {
-    const exchange = this.width;
-    this.width = this.height;
-    this.height = exchange;
-
-    return this;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-
-    return this;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  size;
-
   constructor (size) {
     super(size, size);
     // this.size = size;
