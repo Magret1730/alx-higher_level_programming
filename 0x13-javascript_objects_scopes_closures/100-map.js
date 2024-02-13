6cf8b3714ca634e4d5cc496d04d2bf990c81790e
Magret <oyedeleabiodun79@gmail.com>
2024-02-13 10:39:21 -0800

    Solved checker issues on task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const myList = require('./100-data').list;
-// console.log(myList);
+console.log(myList);
 const newList = myList.map((key, value) => value * key);
 console.log(newList);
