94b073894e51f305f2a93bd2e0579ceac1235b2b
Magret <oyedeleabiodun79@gmail.com>
2024-03-01 11:26:13 -0800

    Solution to the tsak

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to send a request to the specified URL with a header causing the desired response
-curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me"
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
