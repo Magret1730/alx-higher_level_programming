cae8781544e7518967325a85ba3bdfc039db8b32
Magret <belloabiodun17@gmail.com>
2024-03-01 08:04:27 -0800

    Solved checkers issuess

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,9 +10,8 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    try:
-        r = requests.get(url)
+    r = requests.get(url)
+    if r.status_code <= 400:
         print(r.text)
-    except requests.HTTPError as e:
-        print(f"Error code: {e.status_code}")
+    else:
+        print(f"Error code: {r.status_code}")
