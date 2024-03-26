f060789a41dc598297ae9c6118f5283a6cd5db13
Magret <belloabiodun17@gmail.com>
2024-03-26 07:27:57 -0700

    Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+/** script that prints the number of movies where the
+ * character “Wedge Antilles” is present.
+ */
+
+const request = require('request');
+const characterId = 18;
+const StarWarAPIEndpoint = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(StarWarAPIEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch movie data: ${response.statusCode}`);
+  }
+
+  const movies = JSON.parse(body).results; // converting to more readable format
+  let numMovies = 0;
+
+  // Loop through each movie
+  movies.forEach(movie => {
+    // Check if Wedge Antilles is present in the characters list
+    const characters = movie.characters.map(character => parseInt(character.split('/').slice(-2)[0]));
+    if (characters.includes(characterId)) {
+      numMovies++;
+    }
+  });
+
+  console.log(numMovies);
+});
